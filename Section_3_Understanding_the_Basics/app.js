4fc385caf73598aa0e6181aeecf0833445025eff
SauBanh <saubanh05062001@gmail.com>
2023-04-19 23:50:34 +0700

    Blocking Code

--- Section_3_Understanding_the_Basics/app.js
+++ Section_3_Understanding_the_Basics/app.js
@@ -22,14 +22,15 @@ const server = http.createServer((req, res) => {
             console.log(chuck);
             body.push(chuck);
         });
-        req.on("end", () => {
+        return req.on("end", () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split("=")[1];
-            fs.writeFileSync("message.txt", message);
+            fs.writeFile("message.txt", message, (err) => {
+                res.statusCode = 302;
+                res.setHeader("Location", "/");
+                return res.end();
+            });
         });
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
     }
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
