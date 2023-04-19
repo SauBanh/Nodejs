17fb0ecc182008dcc836e22bcd4a54e50a6ae706
SauBanh <saubanh05062001@gmail.com>
2023-04-20 00:29:25 +0700

    using the node modules system

--- Section_3_Understanding_the_Basics/app.js
+++ Section_3_Understanding_the_Basics/app.js
@@ -1,45 +1,13 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
+
+console.log(routes.someText);
+
 // function rqListener(req, res){
 
 // }
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === "/") {
-        res.write("<html>");
-        res.write("<head><title>Enter Message</title></head>");
-        res.write(
-            "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
-        );
-        res.write("</html>");
-        return res.end();
-    }
-    if (url === "/message" && method === "POST") {
-        const body = [];
-        req.on("data", (chuck) => {
-            console.log(chuck);
-            body.push(chuck);
-        });
-        return req.on("end", () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split("=")[1];
-            fs.writeFile("message.txt", message, (err) => {
-                res.statusCode = 302;
-                res.setHeader("Location", "/");
-                return res.end();
-            });
-        });
-    }
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Tuấn Anh đẹp trai</title></head>");
-    res.write(
-        "<body><h1>Dòng này được viết bởi res.write của nodejs</h1></body>"
-    );
-    res.write("</html>");
-    res.end(); // sau khi kết thúc sẽ không thể write bất cứ cái gì vào được nữa
-});
+// const server = http.createServer(routes);
+const server = http.createServer(routes.handler);
 
 server.listen(5657);
