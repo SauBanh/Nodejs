a1e6ca0fdcefec636f5e90d62c0a7c2e032931f3
SauBanh <saubanh05062001@gmail.com>
2023-04-19 21:38:17 +0700

    create a node server

--- Section_2_Optional_JavaScript_Quicl_Refesher/play.js
+++ Section_2_Optional_JavaScript_Quicl_Refesher/play.js
@@ -1,24 +1,7 @@
-const name = "Nguyễn Tuấn Anh";
-let age = 22;
-const handsome = true;
-
-// name = "123"; //const không được phép gắn giá trị khác vì const không thay đổi
-age = 30;
-
-const infoUser = (userName, userAge, handsome) => {
-    return (
-        "Tên: " +
-        userName +
-        ", Tuổi: " +
-        userAge +
-        ", có đẹp trai không: " +
-        handsome
-    );
+const user = {
+    name: "Nguyễn Tuấn Anh",
+    age: 22,
+    handsome: true,
 };
 
-// const sum = (a, b) => a + b;
-const sum = (a) => a + 1;
-
-console.log(sum(5));
-
-console.log(infoUser(name, age, handsome));
+console.log(user);
