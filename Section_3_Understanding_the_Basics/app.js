fb1de1e31ca9410308e53c1742f965e0be7313b4
SauBanh <saubanh05062001@gmail.com>
2023-04-19 22:26:38 +0700

    sending responses

--- Section_3_Understanding_the_Basics/app.js
+++ Section_3_Understanding_the_Basics/app.js
@@ -7,6 +7,14 @@ const http = require("http");
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     // process.exit();
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Tuấn Anh đẹp trai</title></head>");
+    res.write(
+        "<body><h1>Dòng này được viết bởi res.write của nodejs</h1></body>"
+    );
+    res.write("</html>");
+    res.end(); // sau khi kết thúc sẽ không thể write bất cứ cái gì vào được nữa
 });
 
 server.listen(5657);
