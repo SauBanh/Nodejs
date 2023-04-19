62ebffa4391b38a14e3c036f2a0e975ce8fef775
SauBanh <saubanh05062001@gmail.com>
2023-04-19 18:58:18 +0700

    let and const

--- Section_2_Optional_JavaScript_Quicl_Refesher/play.js
+++ Section_2_Optional_JavaScript_Quicl_Refesher/play.js
@@ -2,7 +2,7 @@ const name = "Nguyễn Tuấn Anh";
 let age = 22;
 const handsome = true;
 
-// name = "123"; const không được phép gắn giá trị khác vì const không thay đổi
+name = "123"; //const không được phép gắn giá trị khác vì const không thay đổi
 age = 30;
 
 function infoUser(userName, userAge, handsome) {
