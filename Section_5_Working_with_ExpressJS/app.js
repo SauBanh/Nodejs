94e994621eb73da5f403b564259c933cf9545fa9
SauBanh <saubanh05062001@gmail.com>
2023-04-20 10:03:40 +0700

    handling different routes

--- Section_5_Working_with_ExpressJS/app.js
+++ Section_5_Working_with_ExpressJS/app.js
@@ -1,4 +1,4 @@
-const http = require("http");
+// const http = require("http");
 // const routes = require("./routes");
 const express = require("express"); //đặt biến cho thư viện
 
@@ -11,15 +11,16 @@ const express = require("express"); //đặt biến cho thư viện
 // const server = http.createServer(routes);
 // const server = http.createServer(routes.handler);
 const app = express(); // dùng app để chạy express dưới dạng hàm
-app.use((req, res, next) => {
-    console.log('I"m Sáu Bảnh');
-    next();
+app.use("/add-product", (req, res, next) => {
+    console.log("Add Product Page");
+    res.send("<h1>Add Product Page</h1>");
+    // next();
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
     console.log('I"m Tuấn Anh');
     res.send("<h1>Hello Nodejs</h1>");
 });
 
-const server = http.createServer(app);
-server.listen(5657);
+// const server = http.createServer(app);
+app.listen(5657);
