5d8fa1a920504ff87872c64f28457bdc9ceec2f8
SauBanh <saubanh05062001@gmail.com>
2023-04-20 10:28:12 +0700

    parsing incoming request

--- Section_5_Working_with_ExpressJS/app.js
+++ Section_5_Working_with_ExpressJS/app.js
@@ -1,6 +1,7 @@
 // const http = require("http");
 // const routes = require("./routes");
 const express = require("express"); //đặt biến cho thư viện
+const bodyParser = require("body-parser");
 
 // console.log(routes.someText);
 
@@ -11,14 +12,29 @@ const express = require("express"); //đặt biến cho thư viện
 // const server = http.createServer(routes);
 // const server = http.createServer(routes.handler);
 const app = express(); // dùng app để chạy express dưới dạng hàm
+
+// app.use("/", (req, res, next) => {
+//     // console.log('This always run');
+//     // res.send("<h1>Hello Nodejs</h1>");
+//     next();
+// });
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, next) => {
-    console.log("Add Product Page");
-    res.send("<h1>Add Product Page</h1>");
+    // console.log("Add Product Page");
+    res.send(
+        "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
+    );
     // next();
 });
 
+app.use("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
+});
+
 app.use("/", (req, res, next) => {
-    console.log('I"m Tuấn Anh');
+    // console.log('I"m Tuấn Anh');
     res.send("<h1>Hello Nodejs</h1>");
 });
 
