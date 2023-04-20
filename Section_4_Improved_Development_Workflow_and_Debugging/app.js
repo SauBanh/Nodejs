342d7f8c82625c8b346723de95c23f61de9b1cf9
SauBanh <saubanh05062001@gmail.com>
2023-04-20 08:41:23 +0700

    understanding npm scripts

---
+++ Section_4_Improved_Development_Workflow_and_Debugging/app.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const routes = require("./routes");
+
+console.log(routes.someText);
+
+// function rqListener(req, res){
+
+// }
+
+// const server = http.createServer(routes);
+const server = http.createServer(routes.handler);
+
+server.listen(5657);
