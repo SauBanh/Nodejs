c6a219fce527f634bc7789d5986756e3029ea487
SauBanh <saubanh05062001@gmail.com>
2023-04-20 11:02:14 +0700

    using express router

--- Section_5_Working_with_ExpressJS/app.js
+++ Section_5_Working_with_ExpressJS/app.js
@@ -1,6 +1,8 @@
 // const http = require("http");
 // const routes = require("./routes");
 const express = require("express"); //đặt biến cho thư viện
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
 const bodyParser = require("body-parser");
 
 // console.log(routes.someText);
@@ -13,30 +15,35 @@ const bodyParser = require("body-parser");
 // const server = http.createServer(routes.handler);
 const app = express(); // dùng app để chạy express dưới dạng hàm
 
-// app.use("/", (req, res, next) => {
-//     // console.log('This always run');
-//     // res.send("<h1>Hello Nodejs</h1>");
-//     next();
-// });
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-    // console.log("Add Product Page");
-    res.send(
-        "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
-    );
-    // next();
-});
+app.use(shopRouter);
+app.use(adminRouter);
 
-app.use("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
-});
+// app.use("/", (req, res, next) => {
+//     // console.log('This always run');
+//     res.send("<h1>Hello Nodejs</h1>");
+//     next();
+// });
 
-app.use("/", (req, res, next) => {
-    // console.log('I"m Tuấn Anh');
-    res.send("<h1>Hello Nodejs</h1>");
-});
+// router.use("/add-product", (req, res, next) => {
+//     // console.log("Add Product Page");
+//     res.send(
+//         "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
+//     );
+//     // next();
+// });
+
+// router.post("/product", (req, res, next) => {
+//     // console.log(req.body);
+//     res.redirect("/");
+// });
+
+// app.use("/", (req, res, next) => {
+//     // console.log('I"m Tuấn Anh');
+//     res.send("<h1>Hello Nodejs</h1>");
+// });
 
 // const server = http.createServer(app);
+
 app.listen(5657);
