1b8f9093d6d4b0e22660cd3b7fa7687a77db1dd2
SauBanh <saubanh05062001@gmail.com>
2023-04-21 11:36:27 +0700

    create controller

--- Section_7_The_Model_View_Controller_(MVC)/app.js
+++ Section_7_The_Model_View_Controller_(MVC)/app.js
@@ -1,61 +1,23 @@
-// const http = require("http");
-// const routes = require("./routes");
-const express = require("express"); //đặt biến cho thư viện
 const path = require("path");
-const adminRouter = require("./routes/admin");
-const shopRouter = require("./routes/shop");
+
+const express = require("express"); //đặt biến cho thư viện
 const bodyParser = require("body-parser");
 
-// console.log(routes.someText);
-
-// function rqListener(req, res){
-
-// }
-
-// const server = http.createServer(routes);
-// const server = http.createServer(routes.handler);
 const app = express(); // dùng app để chạy express dưới dạng hàm
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+const adminRouters = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+const errorController = require("./controllers/error");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRouter.routers);
+app.use("/admin", adminRouters);
 app.use(shopRouter);
 
-// app.use("/", (req, res, next) => {
-//     // console.log('This always run');
-//     res.send("<h1>Hello Nodejs</h1>");
-//     next();
-// });
-
-// router.use("/add-product", (req, res, next) => {
-//     // console.log("Add Product Page");
-//     res.send(
-//         "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
-//     );
-//     // next();
-// });
-
-// router.post("/product", (req, res, next) => {
-//     // console.log(req.body);
-//     res.redirect("/");
-// });
-
-// app.use("/", (req, res, next) => {
-//     // console.log('I"m Tuấn Anh');
-//     res.send("<h1>Hello Nodejs</h1>");
-// });
-
-// const server = http.createServer(app);
-
-app.use((req, res, next) => {
-    res.status(404).render("404", {
-        title: "Not Found",
-        pageTitle: "Page not found",
-    });
-});
+app.use(errorController.get404);
 
 app.listen(5657);
--- Section_7_The_Model_View_Controller_(MVC)/routes/shop.js
+++ Section_7_The_Model_View_Controller_(MVC)/routes/shop.js
@@ -1,16 +1,11 @@
 const path = require("path");
+
 const express = require("express");
-const rootDir = require("../util/path");
-const adminData = require("./admin");
+
+const productController = require("../controllers/products");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    // res.send("<h1>Hello from Express</h1>");
-    // console.log(adminData.products);
-    // res.sendFile(path.join(rootDir, "views", "shop.html"));
-    const products = adminData.products;
-    res.render("shop", { prods: products, pageTitle: "My Shop", path: "/" });
-});
+router.get("/", productController.getProducts);
 
 module.exports = router;
