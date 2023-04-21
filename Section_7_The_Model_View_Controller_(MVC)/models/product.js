75e890bd862ec2787b22784cdd5e1a44d74d5666
SauBanh <saubanh05062001@gmail.com>
2023-04-21 13:45:37 +0700

    fetching data in the file data json

--- Section_7_The_Model_View_Controller_(MVC)/models/product.js
+++ Section_7_The_Model_View_Controller_(MVC)/models/product.js
@@ -1,15 +1,43 @@
-const products = [];
+const { error } = require("console");
+const fs = require("fs"); // ghi file
+const path = require("path"); // tạo đường dẫn động
 
-module.exports = class Produc {
+// const products = [];
+
+module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
 
     save() {
-        products.push(this);
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            "data",
+            "products.json"
+        );
+        fs.readFile(p, (err, flieContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(flieContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            "data",
+            "products.json"
+        );
+        fs.readFile(p, (err, flieContent) => {
+            if (err) {
+                return cb([]);
+            }
+            cb(JSON.parse(flieContent));
+        });
     }
 };
